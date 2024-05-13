47d59bd109be977b3ff1f2eb608fcdda8f6e18f2
luhavis <luhavis.dev@gmail.com>
2024-05-13 16:40:30 +0900

    change train port

--- train_ms.py
+++ train_ms.py
@@ -33,7 +33,7 @@ def main():
 
     n_gpus = torch.cuda.device_count()
     os.environ["MASTER_ADDR"] = "localhost"
-    os.environ["MASTER_PORT"] = "80000"
+    os.environ["MASTER_PORT"] = "60000"
 
     hps = utils.get_hparams()
     mp.spawn(
