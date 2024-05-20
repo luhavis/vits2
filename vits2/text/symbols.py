e1475e6675f26221ee056e2810f5f353a9849217
luhavis <luhavis.dev@gmail.com>
2024-05-20 12:15:18 +0900

    add korean symbols

--- vits2/text/symbols.py
+++ vits2/text/symbols.py
@@ -3,14 +3,61 @@
 """
 Defines the set of symbols used in text input to the model.
 """
-_pad = "_"
-_punctuation = ';:,.!?¡¿—…"«»“” '
-_letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
-_letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+pad = ["_"]
+# _punctuation = ';:,.!?¡¿—…"«»“” '
+punctuation = ["!", "?", "…", ",", ".", "'", "-", "¿", "¡", " "]
+letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
+letters_ipa = "ɑɐɒæɓʙβɔɕçɗɖðʤəɘɚɛɜɝɞɟʄɡɠɢʛɦɧħɥʜɨɪʝɭɬɫɮʟɱɯɰŋɳɲɴøɵɸθœɶʘɹɺɾɻʀʁɽʂʃʈʧʉʊʋⱱʌɣɤʍχʎʏʑʐʒʔʡʕʢǀǁǂǃˈˌːˑʼʴʰʱʲʷˠˤ˞↓↑→↗↘'̩'ᵻ"
+others = ["SP", "UNK"]
 
+korean_symbols = ["(", ")", "~", "\\", "[", "]", "/", "^", ":", "ㄸ", "*"]
+
+_jamo_leads = [chr(_) for _ in range(0x1100, 0x1113)]
+_jamo_vowels = [chr(_) for _ in range(0x1161, 0x1176)]
+_jamo_tails = [chr(_) for _ in range(0x11A8, 0x11C3)]
+
+_new_korean_sybols = [
+    "ㄱ",
+    "ㄴ",
+    "ㄷ",
+    "ㄹ",
+    "ㅁ",
+    "ㅂ",
+    "ㅅ",
+    "ㅇ",
+    "ㅈ",
+    "ㅊ",
+    "ㅋ",
+    "ㅌ",
+    "ㅍ",
+    "ㅎ",
+    "ㄲ",
+    "ㄸ",
+    "ㅃ",
+    "ㅆ",
+    "ㅉ",
+    "ㅏ",
+    "ㅓ",
+    "ㅗ",
+    "ㅜ",
+    "ㅡ",
+    "ㅣ",
+    "ㅐ",
+    "ㅔ",
+]
 
 # Export all symbols
-symbols = [_pad] + list(_punctuation) + list(_letters) + list(_letters_ipa)
+symbols = (
+    pad
+    + list(punctuation)
+    + list(letters)
+    + list(letters_ipa)
+    + korean_symbols
+    + _jamo_leads
+    + _jamo_vowels
+    + _jamo_tails
+    + _new_korean_sybols
+)
 
 # Special symbol ids
 SPACE_ID = symbols.index(" ")
